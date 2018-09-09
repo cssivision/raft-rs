62053d7cd2c7c3b5817be18f3725cca2f6d2448f
cssivision <cssivision@gmail.com>
2018-09-09 18:41:09 +0800

    test snapshot

--- tests/cases/test_raft_snap.rs
+++ tests/cases/test_raft_snap.rs
@@ -48,3 +48,63 @@ fn test_pending_snapshot_pause_replcation() {
     let msgs: Vec<Message> = sm.msgs.drain(..).collect();
     assert!(msgs.is_empty());
 }
+
+#[test]
+fn test_snapshot_failure() {
+    let mut sm = new_test_raft(1, vec![1, 2], 10, 1, MemStorage::new());
+    sm.restore(test_snapshot());
+    sm.become_candidate();
+    sm.become_leader();
+
+    sm.prs.get_mut(&2).unwrap().next = 1;
+    sm.prs.get_mut(&2).unwrap().become_snapshot(11);
+
+    let mut m = Message::new();
+    m.set_from(2);
+    m.set_to(1);
+    m.set_msg_type(MessageType::MsgSnapStatus);
+    m.set_reject(true);
+    let _ = sm.step(m);
+    assert_eq!(sm.prs.get(&2).unwrap().pending_snapshot, 0);
+    assert_eq!(sm.prs.get(&2).unwrap().next, 1);
+    assert!(sm.prs.get(&2).unwrap().paused);
+}
+
+#[test]
+fn test_snapshot_succeed() {
+    let mut sm = new_test_raft(1, vec![1, 2], 10, 1, MemStorage::new());
+    sm.restore(test_snapshot());
+    sm.become_candidate();
+    sm.become_leader();
+
+    sm.prs.get_mut(&2).unwrap().next = 1;
+    sm.prs.get_mut(&2).unwrap().become_snapshot(11);
+    let mut m = Message::new();
+    m.set_from(2);
+    m.set_to(1);
+    m.set_msg_type(MessageType::MsgSnapStatus);
+    m.set_reject(false);
+    let _ = sm.step(m);
+    assert_eq!(sm.prs.get(&2).unwrap().pending_snapshot, 0);
+    assert_eq!(sm.prs.get(&2).unwrap().next, 12);
+    assert!(sm.prs.get(&2).unwrap().paused);
+}
+
+#[test]
+fn test_snapshot_abort() {
+    let mut sm = new_test_raft(1, vec![1, 2], 10, 1, MemStorage::new());
+    sm.restore(test_snapshot());
+    sm.become_candidate();
+    sm.become_leader();
+
+    sm.prs.get_mut(&2).unwrap().next = 1;
+    sm.prs.get_mut(&2).unwrap().become_snapshot(11);
+    let mut m = Message::new();
+    m.set_from(2);
+    m.set_to(1);
+    m.set_msg_type(MessageType::MsgAppResp);
+    m.set_index(11);
+    let _ = sm.step(m);
+    assert_eq!(sm.prs.get(&2).unwrap().pending_snapshot, 0);
+    assert_eq!(sm.prs.get(&2).unwrap().next, 12);
+}
