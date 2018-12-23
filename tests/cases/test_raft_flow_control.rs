47bfa5ffa8fd5b196d05cca9780eb2f87db56110
cssivision <cssivision@gmail.com>
2018-12-23 20:54:59 +0800

    test app flow control

--- tests/cases/test_raft_flow_control.rs
+++ tests/cases/test_raft_flow_control.rs
@@ -1,13 +1,176 @@
 use cases::test_raft::new_test_raft;
+use libraft::raftpb::{Entry, Message, MessageType};
 use libraft::storage::{MemStorage, Storage};
+use protobuf::RepeatedField;
 
 // ensure
 // 1. msgApp can fill the sending window until full
 // 2. when the window is full, no more msgApp can be sent.
+// when in replicated state and inflights is full, progress will be paused.
 #[test]
 fn test_msg_app_flow_control_full() {
     let mut r = new_test_raft(1, vec![1, 2], 5, 1, MemStorage::new());
     r.become_candidate();
     r.become_leader();
     r.prs.get_mut(&2).unwrap().become_replicate();
+
+    for _ in 0..r.max_inflight {
+        let mut m = Message::new();
+        m.set_from(1);
+        m.set_to(1);
+        m.set_msg_type(MessageType::MsgProp);
+        let mut e = Entry::new();
+        e.set_data(Vec::from("somedata"));
+        m.set_entries(RepeatedField::from_vec(vec![e]));
+        let _ = r.step(m);
+        let ms: Vec<Message> = r.msgs.drain(..).collect();
+        assert_eq!(ms.len(), 1);
+    }
+
+    assert!(r.prs.get(&2).unwrap().ins.full());
+
+    for _ in 0..10 {
+        let mut m = Message::new();
+        m.set_from(1);
+        m.set_to(1);
+        m.set_msg_type(MessageType::MsgProp);
+        let mut e = Entry::new();
+        e.set_data(Vec::from("somedata"));
+        m.set_entries(RepeatedField::from_vec(vec![e]));
+        let _ = r.step(m);
+        let ms: Vec<Message> = r.msgs.drain(..).collect();
+        assert_eq!(ms.len(), 0);
+    }
+}
+
+#[test]
+fn test_msg_app_flow_control_move_forward() {
+    let mut r = new_test_raft(1, vec![1, 2], 5, 1, MemStorage::new());
+    r.become_candidate();
+    r.become_leader();
+    r.prs.get_mut(&2).unwrap().become_replicate();
+
+    for _ in 0..r.max_inflight {
+        let mut m = Message::new();
+        m.set_from(1);
+        m.set_to(1);
+        m.set_msg_type(MessageType::MsgProp);
+        let mut e = Entry::new();
+        e.set_data(Vec::from("somedata"));
+        m.set_entries(RepeatedField::from_vec(vec![e]));
+        let _ = r.step(m);
+        let ms: Vec<Message> = r.msgs.drain(..).collect();
+        assert_eq!(ms.len(), 1);
+    }
+
+    // 1 is noop, 2 is the first proposal we just sent.
+	// so we start with 2.
+    for i in 2..r.max_inflight {
+        // move forward the window
+        let mut m = Message::new();
+        m.set_from(2);
+        m.set_to(1);
+        m.set_msg_type(MessageType::MsgAppResp);
+        m.set_index(i);
+        let _ = r.step(m);
+        let _: Vec<Message> = r.msgs.drain(..).collect();
+
+        // fill in the inflights window again
+        {
+            let mut m = Message::new();
+            m.set_from(1);
+            m.set_to(1);
+            m.set_msg_type(MessageType::MsgProp);
+            let mut e = Entry::new();
+            e.set_data(Vec::from("somedata"));
+            m.set_entries(RepeatedField::from_vec(vec![e]));
+            let _ = r.step(m);
+            let ms: Vec<Message> = r.msgs.drain(..).collect();
+            assert_eq!(ms.len(), 1);
+        }
+
+        assert!(r.prs.get(&2).unwrap().ins.full());
+
+         for ii in 0..i {
+            let mut m = Message::new();
+            m.set_from(2);
+            m.set_to(1);
+            m.set_msg_type(MessageType::MsgAppResp);
+            m.set_index(ii);
+            let _ = r.step(m);
+            let _: Vec<Message> = r.msgs.drain(..).collect();
+            assert!(r.prs.get(&2).unwrap().ins.full());
+        }
+    }
+}
+
+// ensures a heartbeat response
+// frees one slot if the window is full.
+#[test]
+fn test_msg_app_flow_control_recv_heartbeat() {
+    let mut r = new_test_raft(1, vec![1, 2], 5, 1, MemStorage::new());
+    r.become_candidate();
+    r.become_leader();
+    r.prs.get_mut(&2).unwrap().become_replicate();
+
+    for _ in 0..r.max_inflight {
+        let mut m = Message::new();
+        m.set_from(1);
+        m.set_to(1);
+        m.set_msg_type(MessageType::MsgProp);
+        let mut e = Entry::new();
+        e.set_data(Vec::from("somedata"));
+        m.set_entries(RepeatedField::from_vec(vec![e]));
+        let _ = r.step(m);
+        let ms: Vec<Message> = r.msgs.drain(..).collect();
+        assert_eq!(ms.len(), 1);
+    }
+
+    for tt in 0..5 {
+        assert!(r.prs.get_mut(&2).unwrap().ins.full());
+
+        // recv tt msgHeartbeatResp and expect one free slot
+        for _ in 0..tt {
+            let mut m = Message::new();
+            m.set_from(2);
+            m.set_to(1);
+            m.set_msg_type(MessageType::MsgHeartbeatResp);
+            let _ = r.step(m);
+            let _: Vec<Message> = r.msgs.drain(..).collect();
+            assert!(!r.prs.get_mut(&2).unwrap().ins.full());
+
+            // one slot
+            let mut m = Message::new();
+            m.set_from(1);
+            m.set_to(1);
+            m.set_msg_type(MessageType::MsgProp);
+            let mut e = Entry::new();
+            e.set_data(Vec::from("somedata"));
+            m.set_entries(RepeatedField::from_vec(vec![e]));
+            let _ = r.step(m);
+            let ms: Vec<Message> = r.msgs.drain(..).collect();
+            assert_eq!(ms.len(), 1);
+
+            for _ in 0..10 {
+                let mut m = Message::new();
+                m.set_from(1);
+                m.set_to(1);
+                m.set_msg_type(MessageType::MsgProp);
+                let mut e = Entry::new();
+                e.set_data(Vec::from("somedata"));
+                m.set_entries(RepeatedField::from_vec(vec![e]));
+                let _ = r.step(m);
+                let ms: Vec<Message> = r.msgs.drain(..).collect();
+                assert_eq!(ms.len(), 0);
+            }
+
+            // clear all pending messages.
+            let mut m = Message::new();
+            m.set_from(2);
+            m.set_to(1);
+            m.set_msg_type(MessageType::MsgHeartbeatResp);
+            let _ = r.step(m);
+            let _: Vec<Message> = r.msgs.drain(..).collect();
+        }
+    }
 }
--- tests/cases/test_raft_paper.rs
+++ tests/cases/test_raft_paper.rs
@@ -1,2 +1,48 @@
-#[cfg(test)]
-mod test {}
+use libraft::raft::StateType;
+use cases::test_raft::new_test_raft;
+use libraft::storage::{MemStorage, Storage};
+use libraft::raftpb::{Message, MessageType};
+
+// tests that if one server’s current term is
+// smaller than the other’s, then it updates its current term to the larger
+// value. If a candidate or leader discovers that its term is out of date,
+// it immediately reverts to follower state.
+// Reference: section 5.1
+fn update_term_from_message(state: StateType) {
+    let mut r = new_test_raft(1, vec![1, 2, 3], 10, 1, MemStorage::new());
+    match state {
+        StateType::Follower => {
+            r.become_follower(1, 2);
+        },
+        StateType::Candidate => {
+            r.become_candidate();
+        },
+        StateType::Leader => {
+            r.become_candidate();
+            r.become_leader();
+        },
+        _ => {}
+    }
+
+    let mut m = Message::new();
+    m.set_msg_type(MessageType::MsgApp);
+    m.set_term(2);
+    let _ = r.step(m);
+    assert_eq!(r.term, 2);
+    assert_eq!(r.state, StateType::Follower);
+}
+
+#[test]
+fn test_follower_update_term_from_message() {
+    update_term_from_message(StateType::Follower);
+}
+
+#[test]
+fn test_candidate_update_term_from_message() {
+    update_term_from_message(StateType::Candidate);
+}
+
+#[test]
+fn test_leader_update_term_from_message() {
+    update_term_from_message(StateType::Leader);
+}
